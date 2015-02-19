b2378db5cd0520dabefae56baff00c9227a15d24
dfenjves <fenjves.daniel@gmail.com>
2015-02-19 10:48:07 -0500

    level 2 tests

--- spec/errors_level_1_spec.rb
+++ spec/errors_level_1_spec.rb
@@ -1,2 +1,26 @@
-require './spec_helper'
+require_relative './spec_helper.rb'
 
+describe '#happy_and_i_know_it' do
+	it 'returns a string about hand clapping' do
+		expect(happy_and_i_know_it).to eq("clapping my hands")
+	end
+end
+
+describe '#boom_n_times' do
+	it 'prints "boom" the number of times given as an argument' do
+		expect{boom_n_times(2)}.to output("boom!\nboom!\n").to_stdout
+	end
+end
+
+describe '#upcase_odd_numbered_words' do
+	it 'upcases odd words and leaves even words alone' do
+		expect(upcase_odd_numbered_words("It was the best of times, it was the worst of times")).to eq("It WAS THE best of times, it WAS THE WORST of TIMES")
+	end
+end
+
+
+describe '#sum_array' do
+	it 'returns the sum of an array given as an argument' do
+		expect(sum_array([1,2,3,4,5])).to eq(15)
+	end
+end
--- spec/errors_level_2_spec.rb
+++ spec/errors_level_2_spec.rb
@@ -1,1 +1,32 @@
-require './spec_helper'
+require_relative './spec_helper.rb'
+
+describe 'Video' do
+    it 'creates a new instance of Video with url and name' do
+        keyboard_cat = Video.new("https://www.youtube.com/watch?v=J---aiyznGQ", "Keyboard Cat")
+        expect(keyboard_cat).to be_an_instance_of(Video)
+    end
+
+    it 'rejects a new instance without arguments' do
+        expect{ Video.new }.to raise_error(ArgumentError)
+    end
+
+    it 'adds a new tag to tags array' do
+        keyboard_cat = Video.new("https://www.youtube.com/watch?v=J---aiyznGQ", "Keyboard Cat")
+        keyboard_cat.add_to_tags("animals")
+        expect(keyboard_cat.tags).to eq(["animals"])
+    end
+
+
+    it 'rejects a duplicate tag in tags array' do
+        keyboard_cat = Video.new("https://www.youtube.com/watch?v=J---aiyznGQ", "Keyboard Cat")
+        keyboard_cat.add_to_tags("animals")
+        expect{keyboard_cat.add_to_tags("animals")}.to output("that tag already exists\n").to_stdout
+    end
+
+    it 'adds one to views when played' do
+        keyboard_cat = Video.new("https://www.youtube.com/watch?v=J---aiyznGQ", "Keyboard Cat")
+        5.times { keyboard_cat.play }
+        expect(keyboard_cat.views).to eq(5)
+    end
+
+end
